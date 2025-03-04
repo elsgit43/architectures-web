0244abc16f54ae9d25e0987ca1681f092f049a93
Elias Salvan <elias.salvan@student-cs.fr>
2025-03-04 16:56:57 +0100

    modif

--- bon-app/src/App.tsx
+++ bon-app/src/App.tsx
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          le GOAT = ELIAS
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=AyMOS5rNx-4"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          NE PAS CLIQUER SUR CE LIEN
         </a>
       </header>
     </div>
